2945a670c40a88060b81c87985a826843404f9d8
Ryan Resella <ryan@codeforamerica.org>
2011-06-22 15:07:42 -0700

    Revert "Added spec for seeclickfix.rb"
    This reverts commit 2310bebcde341b4dda803e96f613affa35b605d2.

--- spec/seeclickfix_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'helper'
-
-describe SeeClickFix do
-  describe ".client" do
-    it "should be a SeeClickFix::Client" do
-      SeeClickFix.client.should be_a SeeClickFix::Client
-    end
-  end
-end
