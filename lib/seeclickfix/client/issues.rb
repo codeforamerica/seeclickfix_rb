a875d4c4d5550bdff2933aeb0cc880e20f8c5bb3
Ryan Resella <ryan@codeforamerica.org>
2011-06-22 16:53:58 -0700

    Imeplemented list issues

--- lib/seeclickfix/client/issues.rb
+++ lib/seeclickfix/client/issues.rb
@@ -1,6 +1,11 @@
 module SeeClickFix
   class Client
     module Issues
+      
+      def list_issues(location, options={})
+             get("api/issues.json?at=#{location}", options)
+      end
+      
     end
   end
 end
