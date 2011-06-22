a3a2aca6411eddb3497fa378811659a5f285db54
Ryan Resella <ryan@codeforamerica.org>
2011-06-22 16:07:22 -0700

    Added spec for list issues

--- spec/seeclickfix/client/issues_spec.rb
+++ spec/seeclickfix/client/issues_spec.rb
@@ -7,5 +7,16 @@ describe SeeClickFix::Client::Issues do
     @client = SeeClickFix::Client.new
   end
 
+  describe ".list_issues" do
+
+     it "should return issues" do
+       stub_get("http://seeclickfix.com/api/issues.json?at=San+Francisco").
+         to_return(:body => fixture("issues_sf.json"))
+       test = @client.list_issues("San Francisco")
+       test.first.status.should == "Open"
+     end
+
+   end
+
 
 end
