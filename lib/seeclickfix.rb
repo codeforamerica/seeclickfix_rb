e579595722e0c77a63a50070357a5cb83d592083
Ryan Resella <ryan@codeforamerica.org>
2011-06-22 15:22:41 -0700

    Added client.rb

--- lib/seeclickfix.rb
+++ lib/seeclickfix.rb
@@ -1,3 +1,23 @@
+require 'seeclickfix/configuration'
+require 'seeclickfix/client'
+
 module SeeClickFix
-  # Your code goes here...
+  extend Configuration
+
+  # Alias for SeeClickFix::Client.new
+  #
+  # @return [SeeClickFix::Client]
+  def self.client(options={})
+    SeeClickFix::Client.new(options)
+  end
+
+  # Delegate to SeeClickFix::Client.new
+  def self.method_missing(method, *args, &block)
+    return super unless client.respond_to?(method)
+    client.send(method, *args, &block)
+  end
+
+  def self.respond_to?(method, include_private=false)
+    client.respond_to?(method, include_private) || super(method, include_private)
+  end
 end
