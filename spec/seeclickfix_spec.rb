02ceaab28d0084a6223c827dbfbcb6bf04ded202
Ryan Resella <ryan@codeforamerica.org>
2011-06-22 15:09:33 -0700

    Added spec for seeclickfix.rb

---
+++ spec/seeclickfix_spec.rb
@@ -0,0 +1,16 @@
+require 'helper'
+
+describe SeeClickFix do
+  describe ".respond_to?" do
+    it "should be true if method exists" do
+      SeeClickFix.respond_to?(:client, true).should be_true
+    end
+  end
+
+  describe ".client" do
+    it "should be a SeeClickFix::Client" do
+      SeeClickFix.client.should be_a SeeClickFix::Client
+    end
+  end
+  
+end
