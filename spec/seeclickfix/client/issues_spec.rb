24e98257ecbf77ed3df0d2aa5edf1524f8933f3e
Ryan Resella <ryan@codeforamerica.org>
2011-06-22 16:53:49 -0700

    Changed spec to check ID rather than status

--- spec/seeclickfix/client/issues_spec.rb
+++ spec/seeclickfix/client/issues_spec.rb
@@ -8,15 +8,16 @@ describe SeeClickFix::Client::Issues do
   end
 
   describe ".list_issues" do
+    before do
+      stub_get("api/issues.json?at=San+Francisco").
+        to_return(:status => 200, :body => fixture("issues_sf.json"))
+    end
 
-     it "should return issues" do
-       stub_get("http://seeclickfix.com/api/issues.json?at=San+Francisco").
-         to_return(:body => fixture("issues_sf.json"))
-       test = @client.list_issues("San Francisco")
-       test.first.status.should == "Open"
-     end
-
-   end
-
+    it "should return a list of issues" do
+      test = @client.list_issues("San Francisco")
+      a_get("api/issues.json?at=San+Francisco").should have_been_made
+      test.first.issue_id.should == 108657
+    end
+  end
 
 end
