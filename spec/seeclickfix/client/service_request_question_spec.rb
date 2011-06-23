c7d8edaf1f0cefc0bf52468c2e1bde7d27881526
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 15:48:40 -0700

    Added spec for questions for service request

--- spec/seeclickfix/client/service_request_question_spec.rb
+++ spec/seeclickfix/client/service_request_question_spec.rb
@@ -4,7 +4,20 @@ require 'helper'
 describe SeeClickFix::Client::ServiceRequestQuestions do
 
   before do
-    @client = SeeClickFix::Client.new
+    @client = SeeClickFix::Client.new(:api_key => "abc123")
+  end
+  
+  describe "service_request_type_questions" do
+    before do
+      stub_get("api/request_types/1/request_type_questions.json").
+        to_return(:status => 200, :body => fixture("request_type_questions.json"))  
+    end
+
+    it "should return all questions for the service request type" do
+      test = @client.service_request_type_questions("1")
+      a_get("api/request_types/1/request_type_questions.json").should have_been_made
+      test.first.question.should == "Your email address?"
+    end
   end
 
 
