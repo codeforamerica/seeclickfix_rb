1750122b3b1e73108cc0ce8d008e26f5857c6014
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 15:11:58 -0700

    Implemented list service request types

--- lib/seeclickfix/client/service_request_types.rb
+++ lib/seeclickfix/client/service_request_types.rb
@@ -1,6 +1,11 @@
 module SeeClickFix
   class Client
     module ServiceRequestTypes
+      
+      def service_request_types(lat,lng,options={})
+        get("api/request_types/at.json?lat=#{lat}&lng=#{lng}", options)
+      end
+      
     end
   end
 end
