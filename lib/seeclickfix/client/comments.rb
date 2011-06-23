7446437962584bca55985977437a77dbd19dd5d5
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 13:44:00 -0700

    Implemented list comments

--- lib/seeclickfix/client/comments.rb
+++ lib/seeclickfix/client/comments.rb
@@ -1,6 +1,11 @@
 module SeeClickFix
   class Client
     module Comments
+      
+      def list_comments(issue, options={})
+        get("api/issues/#{issue}/comments.json", options)
+      end
+      
     end
   end
 end
