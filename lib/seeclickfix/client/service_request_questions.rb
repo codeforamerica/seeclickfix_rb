796d13c099e65b369f3d0d6a4a0b2690a744ebd0
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 15:48:56 -0700

    Implemented list service request by question

--- lib/seeclickfix/client/service_request_questions.rb
+++ lib/seeclickfix/client/service_request_questions.rb
@@ -1,6 +1,9 @@
 module SeeClickFix
   class Client
     module ServiceRequestQuestions
+      def service_request_type_questions(request_type,options={})
+        get("api/request_types/#{request_type}/request_type_questions.json", options)
+      end
     end
   end
 end
