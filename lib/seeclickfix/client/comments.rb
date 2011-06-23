3a531373fc181b16a754b266c31102a3c4ae6de9
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 13:50:20 -0700

    Documented list comments

--- lib/seeclickfix/client/comments.rb
+++ lib/seeclickfix/client/comments.rb
@@ -2,6 +2,14 @@ module SeeClickFix
   class Client
     module Comments
       
+      # Use this query the comments and history of a single issue.
+      #
+      # @param issue [String] The issue number for the query
+      # @param options [Hash] A customizable set of options.
+      # @return [Array<Hashie::Mash>] 
+      # @see http://help.seeclickfix.com/kb/api/listing-comments-and-issue-history
+      # @example
+      #   SeeClickFix.list_comments("1050")
       def list_comments(issue, options={})
         get("api/issues/#{issue}/comments.json", options)
       end
