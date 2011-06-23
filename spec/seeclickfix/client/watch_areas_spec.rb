a54a7e2e84e56aad7d246292eb0b1e3866e2e93f
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 14:06:25 -0700

    Added specs for watch areas

--- spec/seeclickfix/client/watch_areas_spec.rb
+++ spec/seeclickfix/client/watch_areas_spec.rb
@@ -7,5 +7,17 @@ describe SeeClickFix::Client::WatchAreas do
     @client = SeeClickFix::Client.new
   end
 
+  describe ".watch_areas" do
+    before do
+      stub_get("api/watchers/list.json?at=San+Francisco").
+        to_return(:status => 200, :body => fixture("watchers.json"))
+    end
 
+    it "should return all the watch areas for the area" do
+      test = @client.watch_areas({:at => "San Francisco"})
+      a_get("api/watchers/list.json?at=San+Francisco").should have_been_made
+      test.first.title.should == "21 Y Peralta Hacienda Neighborhood"
+      test.first.id.should == 936
+    end
+  end
 end
