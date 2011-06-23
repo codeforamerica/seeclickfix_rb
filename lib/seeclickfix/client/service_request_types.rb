f11c2fbf02b2fc152d44320f89f6445cf2af29b5
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 15:21:37 -0700

    Added documentation for list service request types

--- lib/seeclickfix/client/service_request_types.rb
+++ lib/seeclickfix/client/service_request_types.rb
@@ -2,6 +2,16 @@ module SeeClickFix
   class Client
     module ServiceRequestTypes
       
+      # Use this query to list valid service request types (things like pothole, graffiti, street light out etc.) for a given location.
+      #
+      # @param lat [String] Latitude to check for service request types.
+      # @param long [String] Longitude to check for service request types.
+      # @option api_key [String] Latitude to check for service request types.
+      # @param options [Hash] A customizable set of options.
+      # @return [Array<Hashie::Mash>] 
+      # @see http://help.seeclickfix.com/kb/api/list-service-request-types
+      # @example
+      #   SeeClickFix.service_request_types("32.1610815588945","-110.87677001953125")
       def service_request_types(lat,lng,options={})
         get("api/request_types/at.json?lat=#{lat}&lng=#{lng}", options)
       end
