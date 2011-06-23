0273ff11621354893a4aee843e9201e40eb1265f
Ryan Resella <ryan@codeforamerica.org>
2011-06-23 13:41:55 -0700

    Added spec for list commments

--- spec/seeclickfix/client/comments_spec.rb
+++ spec/seeclickfix/client/comments_spec.rb
@@ -7,5 +7,18 @@ describe SeeClickFix::Client::Comments do
     @client = SeeClickFix::Client.new
   end
 
+  describe ".list_comments" do
+    before do
+      stub_get("api/issues/14074/comments.json").
+        to_return(:status => 200, :body => fixture("comments.json"))
+    end
 
+    it "should return all comments for the given issue" do
+      test = @client.list_comments("14074")
+      a_get("api/issues/14074/comments.json").should have_been_made
+      test.first.name.should == "Bruce Alexander"
+      test.first.issue_id.should == 14074
+    end
+  end
+  
 end
