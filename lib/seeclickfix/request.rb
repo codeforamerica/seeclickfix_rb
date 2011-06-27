23b451b28af864e5a3182c3f1ef44b7ffdbca2fa
Ryan Resella <ryan@codeforamerica.org>
2011-06-27 15:03:46 -0700

    Added post method

--- lib/seeclickfix/request.rb
+++ lib/seeclickfix/request.rb
@@ -3,12 +3,22 @@ module SeeClickFix
     def get(path, options={})
       request(:get, path, options)
     end
+    
+    def post(path, options={})
+      request(:post, path, options)
+    end
 
     private
 
     def request(method, path, options)
       response = connection.send(method) do |request|
-        request.url(path, options)
+        case method
+          when :get
+            request.url(path, options)
+          when :post
+            request.path = path
+            request.body = options unless options.empty?
+          end
       end
       response.body
     end
