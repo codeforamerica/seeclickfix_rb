a1882018083af0e2af4c26bc32c4082ea4b82cb3
Ryan Resella <ryan@codeforamerica.org>
2011-06-27 16:15:33 -0700

    Added documnenation for create comments

--- lib/seeclickfix/client/comments.rb
+++ lib/seeclickfix/client/comments.rb
@@ -14,8 +14,23 @@ module SeeClickFix
         get("api/issues/#{issue}/comments.json", options)
       end
       
-      def create_comment(issue_id, comment, comment_email, options={})
-        post("api/issues/#{issue_id}/comments.json?comment[comment]=#{comment}&comment[email]=#{comment_email}", options)
+      # Add a comment to an issue
+      #
+      # @param issue [String] The issue number for the query
+      # @param api_key [String] API Key
+      # @param comment [String] Comment to add
+      # @param comment_email [String] Email of the person making the comment
+      # @param options [Hash] A customizable set of options.
+      # @options options comment[name]  Name of the person making the comment.
+      # @options options comment[send_email] Subscribe the email added to all comments.
+      # @options options comment[youtube_url] URL of a youtube video to add.
+      # @options options comment[comment_image_attributes][uploaded_data] Image to add to the comment.
+      # @return [Array<Hashie::Mash>] 
+      # @see http://help.seeclickfix.com/kb/api/adding-a-comment
+      # @example
+      #   SeeClickFix.create_comment("7000", "hello", "joe@myemail.com")
+      def create_comment(issue, comment, comment_email, options={})
+        post("api/issues/#{issue}/comments.json?comment[comment]=#{comment}&comment[email]=#{comment_email}", options)
       end
       
     end
