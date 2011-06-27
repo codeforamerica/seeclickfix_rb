876135db073d63a75e208d460351b591dc75402e
Ryan Resella <ryan@codeforamerica.org>
2011-06-27 16:04:11 -0700

    Added specs for create comments

--- spec/seeclickfix/client/comments_spec.rb
+++ spec/seeclickfix/client/comments_spec.rb
@@ -21,4 +21,18 @@ describe SeeClickFix::Client::Comments do
     end
   end
   
+  describe ".create_comment" do
+    before do
+      stub_post("api/issues/7000/comments.json?comment[comment]=hello&comment[email]=joe@myemail.com").
+        to_return(:status => 200, :body => fixture("create_comment.json"))
+    end
+
+    it "should create a new comment on the issue" do
+      test = @client.create_comment("7000", "hello", "joe@myemail.com")
+      a_post("api/issues/7000/comments.json?comment[comment]=hello&comment[email]=joe@myemail.com").should have_been_made
+      test.first.comment.should == "hello"
+      test.first.id.should == 41623
+    end
+  end
+  
 end
